600cfcb1db9b18e8b0e1ef79c68cdbc4a7e6e7c6
Sharik18 <62701261+Sharik18@users.noreply.github.com>
2020-03-26 15:14:48 +0300

    Update Loader.java

--- Booleans/src/Loader.java
+++ Booleans/src/Loader.java
@@ -10,12 +10,19 @@ public class Loader {
     int appleCount = 8; // items
 
     //powder - 400 g, sugar - 10 g, milk - 1 l, oil - 30 ml
+    if (powderAmount >= 400 && sugarAmount >= 10 && milkAmount >= 1000 && oilAmount >=30)
+    {
     System.out.println("Pancakes");
-
+    }
     //milk - 300 ml, powder - 5 g, eggs - 5
+    if (powderAmount >= 5 && milkAmount >= 300 && eggsCount >=30)
+    {
     System.out.println("Omelette");
-
+    }
     //apples - 3, milk - 100 ml, powder - 300 g, eggs - 4
+    if (powderAmount >= 300 && appleCount >= 3 && milkAmount >= 100 && eggsCount >=4)
+    {
     System.out.println("Apple pie");
+    }
   }
 }
